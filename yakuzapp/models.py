6fc0a5de768f453a6860516384832673d5845bf4
Julio Branda <kolstomier@protonmail.com>
2023-06-28 13:19:17 -0300

    creada db, modelo entrada y superuser

--- yakuzapp/models.py
+++ yakuzapp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Entrada(models.Model):
+    titulo = models.CharField(max_length=100)
+    imagen = models.ImageField(upload_to=None, height_field=None, width_field=None, max_length=100) 
+    link = models.URLField(null=True, blank=True)
+    descripcion = models.CharField(max_length=300)
+
+    class Meta:
+        verbose_name = 'Entrada'
+        verbose_name_plural = 'Entradas'
+
+    def __str__(self):
+        return self.titulo
--- yakuzapp/views.py
+++ yakuzapp/views.py
@@ -3,4 +3,6 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return render(request, 'yakuzapp/index.html')
+
+    ctx = ""
+    return render(request, 'yakuzapp/index.html', ctx)
